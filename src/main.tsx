e76348db87dd1e64e6dabb98fee7029965e495d3
starthalf <hcgkhlee@gmail.com>
2026-02-02 02:02:22 +0900

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // [1. 라우터 불러오기]
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* [2. 앱 전체를 BrowserRouter로 감싸기] */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
